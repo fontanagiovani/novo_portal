10af0ba380c5311da538de2e10828310a97bb7b0
Giovani J. Fontana <fontanagiovani@gmail.com>
2014-05-14 12:27:46 -0400

    Adição do coverage no projeto

--- portal/settings.py
+++ portal/settings.py
@@ -72,7 +72,17 @@ if DEBUG:
 
     DEBUG_APPS = (
         'debug_toolbar',
+        'django_nose',
     )
+
+    # Use nose to run all tests
+    TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+    # Tell nose to measure coverage on the 'foo' and 'bar' apps
+    NOSE_ARGS = [
+        '--with-coverage',
+        '--cover-package=portal.core',
+    ]
 
     INSTALLED_APPS = DEBUG_APPS_BEFORE_INSTALLED_APPS + INSTALLED_APPS + DEBUG_APPS
 else:
