09934b40a75311fc7c7b6c25455e86a7fadb6b1b
raquelmelo <rcorreiademelo@gmail.com>
2014-06-02 09:43:08 -0400

    alteracao do admin

--- portal/core/admin.py
+++ portal/core/admin.py
@@ -1,37 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from mptt.admin import MPTTModelAdmin
 from django_summernote.admin import SummernoteModelAdmin
-from portal.core.models import Curso
-from portal.core.models import Site
-from portal.core.models import Menu
-from portal.core.models import MenuContainer
 from portal.core.models import Pagina
 from portal.core.models import MidiaPagina
 
-
-class CursoAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Curso, CursoAdmin)
-
-
-class SiteAdmin(MPTTModelAdmin):
-    pass
-
-admin.site.register(Site, SiteAdmin)
-
-
-class MenuAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Menu, MenuAdmin)
-
-
-class MenuContainerAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(MenuContainer, MenuContainerAdmin)
 
 
 class MidiaPaginaInLine(admin.StackedInline):
