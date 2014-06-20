fa766f26a014d46d7ecf7b77b7103af51400ca46
Henrique Bastos <henrique@bastos.net>
2014-06-20 09:26:09 -0300

    Adiciona jetpack, host e fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from fabric.api import task, env
+from unipath import Path
+from jetpack.helpers import Project
+
+# Exposes other functionalities
+from jetpack import setup, deploy, db, config, django, logs
+
+
+# Always run fabric from the repository root dir.
+Path(__file__).parent.chdir()
+
+
+@task
+def production():
+    env.PROJECT = Project(project='portal', instance='production')
+    env.hosts = ['200.129.244.17']
+    env.user = env.PROJECT.user
