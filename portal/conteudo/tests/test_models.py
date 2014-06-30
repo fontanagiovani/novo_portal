222dd0c6181cdde3c0307905bd05ff3e68f5b468
Giovani J. Fontana <fontanagiovani@gmail.com>
2014-06-30 11:06:37 -0400

    Adição dos testes de modelo

--- portal/conteudo/tests/test_models.py
+++ portal/conteudo/tests/test_models.py
@@ -1,1 +1,70 @@
 # -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.utils import timezone
+from portal.conteudo.models import Noticia
+from portal.conteudo.models import Anexo
+from filer.models import File as FileFiler
+from django.core.urlresolvers import reverse
+# from model_mommy import mommy
+
+
+class NoticiaTest(TestCase):
+    def setUp(self):
+        self.obj = Noticia(
+            titulo=u'Título',
+            texto=u'seu texto aqui!!!',
+            data_publicacao=timezone.now(),  # '2014-03-21 17:59:00',
+        )
+
+    def test_criacao(self):
+        """
+        Noticia deve conter titulo, texto, data_publicacao
+        """
+        self.obj.save()
+        self.assertIsNotNone(self.obj.pk)
+
+    def test_unicode(self):
+        """
+        Noticia deve apresentar o titulo como unicode
+        """
+        self.assertEqual(u'Título', unicode(self.obj))
+
+    def test_get_absolute_url(self):
+        """
+        Noticia deve ter um url de acesso direto
+        """
+        self.obj.save()
+        self.assertEqual(reverse('noticia_detalhe', kwargs={'noticia_id': '1'}), self.obj.get_absolute_url())
+
+
+class MidiaTest(TestCase):
+    def setUp(self):
+        self.noticia = Noticia(
+            titulo=u'Título',
+            texto=u'seu texto aqui!!!',
+            data_publicacao=timezone.now(),  # '2014-03-21 17:59:00',
+        )
+        self.noticia.save()
+
+        arquivo = FileFiler()
+        arquivo.save()
+
+        self.anexo = Anexo(
+            noticia=self.noticia,
+            descricao=u'foto1',
+            arquivo=arquivo,
+        )
+
+    def test_criacao(self):
+        """
+        Anexo deve possuir conteudo, descricao e arquivo
+        """
+        self.anexo.save()
+        self.assertIsNotNone(self.anexo.pk)
+
+    def test_unicode(self):
+        """
+        Anexo deve apresentar descricao como unicode
+        """
+        self.assertEqual(u'foto1', unicode(self.anexo))
+
