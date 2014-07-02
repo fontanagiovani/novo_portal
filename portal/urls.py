6fd032943fdb78ac663e0b1d0ceebec5c5dff9ab
Giovani J. Fontana <fontanagiovani@gmail.com>
2014-07-02 13:34:23 -0400

    Separação do urls.py e adequação dos reverse url da app conteudo

--- portal/urls.py
+++ portal/urls.py
@@ -8,8 +8,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^summernote/', include('django_summernote.urls')),
-                       url(r'^$', 'portal.core.views.home', name='home'),
                        url(r'^conteudo/', include('portal.conteudo.urls', namespace='conteudo')),
+                       url(r'^$', 'portal.core.views.home', name='home'),
+                       url(r'^conteudo/(?P<conteudo_id>\d+)/', 'portal.core.views.conteudo_detalhe',
+                           name='conteudo_detalhe'),
+                       url(r'^thumbnail/(?P<conteudo_id>\d+)/', 'portal.core.views.thumbnail', name='thumbnail'),
                        )
 
 # Trecho utilizado para que o django sirva os arquivos do summernote
