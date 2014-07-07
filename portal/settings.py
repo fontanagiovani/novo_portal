9af4643fe19ef1b76a0b01d0e9e5647c58473af0
Giovani J. Fontana <fontanagiovani@gmail.com>
2014-07-07 12:49:54 -0400

    Remoção de código de configuração do database

--- portal/settings.py
+++ portal/settings.py
@@ -129,9 +129,6 @@ DATABASES = {
         cast=db_url),
 }
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
