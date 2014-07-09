58bc19c2892e4cab5aa2e57ad71c8edf59722d37
raquelmelo <rcorreiademelo@gmail.com>
2014-07-09 10:19:56 -0400

    insersao de banners de acesso rapido no template portal

--- portal/banner/models.py
+++ portal/banner/models.py
@@ -1,13 +1,13 @@
 #coding: utf-8
 from django.db import models
-from filer.fields.file import FilerFileField
+from filer.fields.image import FilerImageField
 # Create your models here.
 
 
 class Banner(models.Model):
     titulo=models.CharField(max_length=250, verbose_name=u'Título', null=True, blank=True)
     data_publicacao=models.DateTimeField(verbose_name=u'Data de publicação')
-    arquivo=FilerFileField(related_name='midia_banner')
+    arquivo=FilerImageField(related_name='midia_banner')
 
     class Meta:
         verbose_name = u'Banner'
@@ -21,7 +21,7 @@ class Banner(models.Model):
 class BannerAcessoRapido(models.Model):
     titulo = models.CharField(max_length=250, verbose_name=u'Título')
     data_publicacao = models.DateTimeField(verbose_name=u'Data de publicação')
-    midia_image = FilerFileField(verbose_name=u'Mídia', related_name='ar_banner')
+    midia_image = FilerImageField(verbose_name=u'Mídia', related_name='ar_banner')
 
     class Meta:
         verbose_name = u'Banner de acesso rápido'
@@ -29,8 +29,7 @@ class BannerAcessoRapido(models.Model):
         ordering = ('-data_publicacao', '-id')
 
     def __unicode__(self):
-      return self.midia_image.name
+      return self.titulo
 
-    def primeira_imagem(self):
-        if self.midia_image.filter(arquivo__image__isnull=False).exists():
-            return self.midia_image.filter(arquivo__image__isnull=False)[0].arquivo
+
+
