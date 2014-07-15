3310357a6f776222a636b3a4d781908aaca8bfb6
Eldio Schalm <eldio.schalm@ifmt.edu.br>
2014-07-15 15:40:52 -0400

    bug fix na app guia de cursos

--- portal/cursos/views.py
+++ portal/cursos/views.py
@@ -6,7 +6,7 @@ from portal.cursos.models import Curso, Campus, Grupo_Cursos, Formacao
 
 def listatudo(request):
     formacao = Curso.objects.select_related('Formacao').values('formacao__id', 'formacao__nome').distinct()
-    campi = Curso.objects.select_related('Campus').values('campus__id', 'campus__nome').distinct()
+    campi = Curso.objects.select_related('Campus').values('campus__id', 'formacao__id', 'campus__nome').distinct()
     # grupo_cursos = Curso.objects.select_related('Grupo_Cursos').filter(formacao__nome__in=formacao).values('grupo__id', 'grupo__nome').distinct()
     grupo_cursos = Curso.objects.select_related('Grupo_Cursos').values('formacao__id', 'campus__id', 'grupo__nome').distinct()
     return render(request, 'cursos/listacursos.html', {'formacao': formacao,'grupo_cursos': grupo_cursos, 'campi': campi})
