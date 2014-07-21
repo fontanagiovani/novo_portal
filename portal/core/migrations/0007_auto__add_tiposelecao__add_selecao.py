b1b899f96e7d635d0a7300eb8a468900d19f82fd
Giovani J. Fontana <fontanagiovani@gmail.com>
2014-07-21 14:10:50 -0400

    Alteração nas migrações do modelo de seleção devido à problemas com o postgresql

--- portal/core/migrations/0007_auto__add_tiposelecao__add_selecao.py
+++ portal/core/migrations/0007_auto__add_tiposelecao__add_selecao.py
@@ -8,13 +8,25 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'TipoSelecao'
+        db.create_table(u'core_tiposelecao', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('parent', self.gf('mptt.fields.TreeForeignKey')(blank=True, related_name='pai', null=True, to=orm['core.TipoSelecao'])),
+            ('titulo', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=100)),
+            (u'lft', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            (u'rght', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            (u'tree_id', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            (u'level', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+        ))
+        db.send_create_signal(u'core', ['TipoSelecao'])
+
         # Adding model 'Selecao'
         db.create_table(u'core_selecao', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('tipo', self.gf('django.db.models.fields.CharField')(max_length=4)),
+            ('tipo', self.gf('mptt.fields.TreeForeignKey')(to=orm['core.TipoSelecao'])),
             ('titulo', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=100, blank=True)),
-            ('url', self.gf('django.db.models.fields.CharField')(max_length=250, blank=True)),
+            ('url', self.gf('django.db.models.fields.CharField')(max_length=250)),
             ('status', self.gf('django.db.models.fields.CharField')(max_length=3)),
             ('data_abertura_edital', self.gf('django.db.models.fields.DateTimeField')()),
             ('data_abertura_inscricoes', self.gf('django.db.models.fields.DateTimeField')()),
@@ -25,6 +37,9 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
+        # Deleting model 'TipoSelecao'
+        db.delete_table(u'core_tiposelecao')
+
         # Deleting model 'Selecao'
         db.delete_table(u'core_selecao')
 
@@ -49,11 +64,21 @@ class Migration(SchemaMigration):
             'data_encerramento_inscricoes': ('django.db.models.fields.DateTimeField', [], {}),
             'data_publicacao': ('django.db.models.fields.DateTimeField', [], {}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100', 'blank': 'True'}),
             'status': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
-            'tipo': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'tipo': ('mptt.fields.TreeForeignKey', [], {'to': u"orm['core.TipoSelecao']"}),
             'titulo': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'url': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'})
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'core.tiposelecao': {
+            'Meta': {'ordering': "('titulo',)", 'object_name': 'TipoSelecao'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            u'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'pai'", 'null': 'True', 'to': u"orm['core.TipoSelecao']"}),
+            u'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
         }
     }
 
