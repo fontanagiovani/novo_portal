23f0e93f523613c2eaf8dd4e75a11316f1515c8d
Giovani J. Fontana <fontanagiovani@gmail.com>
2014-07-21 13:37:51 -0400

    Edição na deleção de arquivos gerados no teste

--- portal/core/tests/util.py
+++ portal/core/tests/util.py
@@ -25,11 +25,11 @@ def del_midia_filer(file_name):
             if name.startswith(file_name.lower()):
                 dad_path, midia = os.path.split(os.path.join(root, name))
                 os.remove(os.path.join(root, name))
-                del_recursive_folder(dad_path)
+                # del_recursive_folder(dad_path)
 
     for root, dirs, files in os.walk(settings.__getattr__('FILER_PUBLIC_THUMBNAIL'), topdown=False):
         for name in files:
             if name.startswith(file_name.lower()):
                 dad_path, midia = os.path.split(os.path.join(root, name))
                 os.remove(os.path.join(root, name))
-                del_recursive_folder_thumbnails(dad_path)
+                # del_recursive_folder_thumbnails(dad_path)
