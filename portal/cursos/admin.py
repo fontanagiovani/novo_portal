3323db4a653f96f7e819c2565b8add4118dd4e82
Eldio Schalm <eldio.schalm@ifmt.edu.br>
2014-09-11 16:38:58 -0400

    modificações no models.py do guia de curso, acrescentando descrição no curso e anexo.

--- portal/cursos/admin.py
+++ portal/cursos/admin.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-# from portal.cursos.models import Campus
 from portal.cursos.models import Curso
 from portal.cursos.models import Formacao
 from portal.cursos.models import GrupoCursos
+from portal.cursos.models import AnexoCurso
 
 
 class FormacaoAdmin(admin.ModelAdmin):
     pass
 
 
-# class CampusAdmin(admin.ModelAdmin):
-#     pass
+class AnexoCursoInLine(admin.StackedInline):
+    from django.forms import TextInput, Textarea
+    from django.db import models
+    model = AnexoCurso
+    extra = 1
 
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'115'})},
+        # models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    }
 
-class CursoAdmin(admin.ModelAdmin):
-    pass
+class CursoAdmin(SummernoteModelAdmin):
+    inlines = [AnexoCursoInLine,]
 
 
 class GrupoCursosAdmin(SummernoteModelAdmin):
@@ -24,6 +31,5 @@ class GrupoCursosAdmin(SummernoteModelAdmin):
 
 
 admin.site.register(Formacao, FormacaoAdmin)
-# admin.site.register(Campus, CampusAdmin)
 admin.site.register(Curso, CursoAdmin)
 admin.site.register(GrupoCursos, GrupoCursosAdmin)
