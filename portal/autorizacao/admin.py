4ddde595e33921292376570319370a05ac98b0fc
Giovani J. Fontana <fontanagiovani@gmail.com>
2014-10-22 17:07:39 -0300

    Adição de geração automática de senha na criação de usuários

--- portal/autorizacao/admin.py
+++ portal/autorizacao/admin.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from portal.autorizacao.models import Permissao
 from django.contrib.auth.admin import User
@@ -17,8 +18,19 @@ class SiteInline(admin.StackedInline):
 
 
 class PermissaoAdmin(reversion.VersionAdmin, UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'permissao')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'log_atividade', 'date_joined', 'last_login',
+                    'is_staff', 'permissao')
     inlines = [SiteInline]
+    date_hierarchy = 'last_login'
+
+    def log_atividade(self, obj):
+        if obj.logentry_set.all().exists():
+            return True
+        else:
+            return False
+
+    log_atividade.short_description = u'Log de atividade'
+    log_atividade.boolean = True
 
 admin.site.unregister(User)
 admin.site.register(User, PermissaoAdmin)
