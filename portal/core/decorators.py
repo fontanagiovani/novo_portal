2cd0df722d625d0e8d2ba4d2cce0f0a0c04271dc
Eldio Schalm <eldio.schalm@ifmt.edu.br>
2014-10-31 15:08:09 -0300

    adicionada session no decorator para contagem de url

--- portal/core/decorators.py
+++ portal/core/decorators.py
@@ -6,10 +6,11 @@ from portal.core.models import ContadorVisitas
 def contar_acesso(method):
     @functools.wraps(method)
     def wrapper(request, *args, **kwargs):
-        contador, contador_criado = ContadorVisitas.objects.get_or_create(url=request.path)
-        contador.contagem += 1
-        contador.save()
-
+        if not request.session.get('visitou', False):
+            contador, contador_criado = ContadorVisitas.objects.get_or_create(url=request.path)
+            contador.contagem += 1
+            contador.save()
+            request.session['visitou'] = True
         return method(request, *args, **kwargs)
 
     return wrapper
