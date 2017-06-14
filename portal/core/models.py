684d91ac5e55fae2fb8b34c7dfd6c90e5b46a66e
Eldio Schalm <eldioschalm@hotmail.com>
2017-06-14 14:22:35 -0400

    adicionado related_name para o campo hotsite_background

--- portal/core/models.py
+++ portal/core/models.py
@@ -68,7 +68,7 @@ class SiteDetalhe(models.Model):
     disqus_shortname = models.CharField(max_length=250, null=True, blank=True,
                                         help_text=u'ShortName do site no serviço de comentários DISQUS')
     hotsite = models.BooleanField(default=False)
-    hotsite_background = FilerImageField(null=True, blank=True, verbose_name=u'Background do hotsite')
+    hotsite_background = FilerImageField(null=True, blank=True, related_name='hotsite_background_SiteDetalhe', verbose_name=u'Background do hotsite')
     modal = models.TextField(null=True, blank=True)
     social = models.TextField(null=True, blank=True)
     links_uteis = models.TextField(null=True, blank=True)
