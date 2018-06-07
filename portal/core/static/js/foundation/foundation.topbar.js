e50bc2bbdb8e784a0e3d47489f0b816edc436efe
Eldio Schalm <eldio.schalm@ifmt.edu.br>
2018-06-07 14:56:53 -0400

    mudanças no menu

--- portal/core/static/js/foundation/foundation.topbar.js
+++ portal/core/static/js/foundation/foundation.topbar.js
@@ -10,7 +10,7 @@
       index : 0,
       sticky_class : 'sticky',
       custom_back_text: true,
-      back_text: 'Back',
+      back_text: '↶ Voltar',
       mobile_show_parent_link: true,
       is_hover: true,
       scrolltop : true, // jump to top when sticky nav menu toggle is clicked
